2e6d7e7bae2435f1b39c50e8c822038b373c61ff
Cheryet <C.heryet@outlook.com>
2022-12-14 15:19:59 -0700

    implement http auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with username: "jungle", password: "book"
+
   def index
     @products = Product.order(id: :desc).all
   end
